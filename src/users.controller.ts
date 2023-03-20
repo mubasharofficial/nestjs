ef4eee76bc111f4b03a953bb60622fbbc4c5f658
mubashar <mubashar.hussain22@gmail.com>
2023-03-21 01:38:50 +0500

    Working How to start

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,29 +1,67 @@
-import {Controller,Get,Post,Put,Delete,Patch,Req,HttpCode,HttpStatus} from '@nestjs/common';
-import { Request, response } from 'express';
+import {Controller,Get,Req,Res,HttpCode,HttpStatus,Header,Redirect} from '@nestjs/common';
+import { Request, Response } from 'express';
 import {of} from 'rxjs';
 @Controller('/user')
 export class UsersController{
 
     @Get('/profile')
     @HttpCode(HttpStatus.NO_CONTENT)
-    async getProfile(){
-        return new Promise((resolve,Reject)=>{
-            resolve( {
-                        'result':'Welcome To Pakistan'
-                     }
-            )
-        })
+    getProfile(@Req() req: Request,@Res({passthrough:true}) res: Response)
+    {
+        res.status(200);
+        return {
+                    hello:'World'
+                }
     }
 
+
+
     @Get('/user')
-    userInfo(@Req() req: Request){
-        // observeAble
-        console.log(req)
-        return of({    
-            'id':'121',
-            'name':'Ali',
-            'class':'7th',
-        })
+    @Header('Cache-Controller','none')
+    @Header('X-Name','Mubahar')
+    @HttpCode(200)
+    getUser(@Req() req: Request)
+    {
+        return {
+                    hello:'World'
+                }
+    }
+
+
+    @Get('/reuser')
+    @Redirect('/user/acount',3001)
+    getRedirectUser(@Req() req: Request)
+    {
+        const rn = ~~(Math.random() *10 +1);
+
+        if(rn < 5)
+        {   
+            return {
+                        url:'/user/acount',
+                        statusCode:302
+                    }
+
+        }else{
+
+            return {
+                url:'/user/acount-detail',
+                statusCode:302
+            }
+        }
+
+    }
+
+    @Get('/acount')
+    Redirect()
+    {
+        return "Working Account";
+    }
+
+    
+    @Get('/acount-detail')
+    Account_Detail()
+    {
+        return "account Detail";
     }
 
 }
