a3ee16e0f2d5db4bad8e1d14ceeb6d0c8ed388eb
mubashar <mubashar.hussain22@gmail.com>
2023-03-21 12:22:34 +0500

    start working on branch 6

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-import { AlbumsController } from './album.controller';
 @Module({
-  controllers: [UsersController,AlbumsController],
+  controllers: [UsersController],
 })
 export class AppModule {}
